73a9e7e7ecd8c04073388341bba5fe4e48098b1e
Dmytro <sharavara.d@icloud.com>
2021-04-23 12:34:47 +0200

    promo section added

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.team__slider').slick({
+        arrows: false,
+        dots: true,
+		infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2
+    });
+});
