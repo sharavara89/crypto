d762fb0024b52de3b5c442ed6be7da0f2691cebf
Dmytro <sharavara.d@icloud.com>
2021-04-28 00:28:59 +0200

    modal video added

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,12 +2,41 @@
 
 $(document).ready(function () {
     $('.team__slider').slick({
-        arrows: false,
         dots: true,
-		infinite: true,
+        arrows: false,
+        infinite: false,
+        speed: 300,
+        mobileFirst: true,
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+        responsive: [
+            {
+            breakpoint: 1439,
+            settings: "unslick"
+            },
+            {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+                }
+            }
+        ]
     });
 });
 
-$(".js-modal-btn").modalVideo();
+$(function() {
+    $(".video").click(function () {
+    var theModal = $(this).data("target"),
+    videoSRC = $(this).attr("data-video"),
+    videoSRCauto = videoSRC;
+    $(theModal + ' iframe').attr('src', videoSRCauto);
+    $(theModal + ' button.close').click(function () {
+        $(theModal + ' iframe').attr('src', '');
+    });
+    });
+});
+
+$('#videoModal').on('hidden.bs.modal', function (e) {
+    $('#videoModal').find('iframe').attr('src', '');
+});
