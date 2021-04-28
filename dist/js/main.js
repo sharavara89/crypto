0ddc523c24d76580c5eeb1806f02945c96860d50
Dmytro <sharavara.d@icloud.com>
2021-04-28 13:28:37 +0200

    media finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,20 +23,57 @@ $(document).ready(function () {
             }
         ]
     });
-});
 
-$(function() {
-    $(".video").click(function () {
-    var theModal = $(this).data("target"),
-    videoSRC = $(this).attr("data-video"),
-    videoSRCauto = videoSRC;
-    $(theModal + ' iframe').attr('src', videoSRCauto);
-    $(theModal + ' button.close').click(function () {
-        $(theModal + ' iframe').attr('src', '');
+
+    function validateForms(form) {
+        $(form).validate({
+            rules:{
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                },
+                text: {
+                    required: true,
+                    minlength: 10
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Ведите минимум {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Адрес должен быть в формате example@domain.com"
+                },
+                text: {
+                    required: "Пожалуйста, введите свое сообщение",
+                    minlength: jQuery.validator.format("Ведите минимум {0} символов!")
+                }
+            }
+        });
+    }
+
+
+    $(function() {
+        $(".video").click(function () {
+        var theModal = $(this).data("target"),
+        videoSRC = $(this).attr("data-video"),
+        videoSRCauto = videoSRC;
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', '');
+            });
         });
     });
-});
-
-$('#videoModal').on('hidden.bs.modal', function (e) {
-    $('#videoModal').find('iframe').attr('src', '');
+    
+    $('#videoModal').on('hidden.bs.modal', function (e) {
+        $('#videoModal').find('iframe').attr('src', '');
+    });
 });
--- src/js/main.js
+++ src/js/main.js
@@ -23,20 +23,22 @@ $(document).ready(function () {
             }
         ]
     });
-});
 
-$(function() {
-    $(".video").click(function () {
-    var theModal = $(this).data("target"),
-    videoSRC = $(this).attr("data-video"),
-    videoSRCauto = videoSRC;
-    $(theModal + ' iframe').attr('src', videoSRCauto);
-    $(theModal + ' button.close').click(function () {
-        $(theModal + ' iframe').attr('src', '');
+//Modal video replacing iframe src
+
+    $(function() {
+        $(".video").click(function () {
+        var theModal = $(this).data("target"),
+        videoSRC = $(this).attr("data-video"),
+        videoSRCauto = videoSRC;
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', '');
+            });
         });
     });
-});
-
-$('#videoModal').on('hidden.bs.modal', function (e) {
-    $('#videoModal').find('iframe').attr('src', '');
+    
+    $('#videoModal').on('hidden.bs.modal', function (e) {
+        $('#videoModal').find('iframe').attr('src', '');
+    });
 });
